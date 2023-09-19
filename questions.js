6a043fc0d991eb51c12e9e9bef9e504c4cb2d386
EA - Rafael Rodrigues De Almeida <rafael_almeida01@educadventista.org>
2023-09-19 10:43:07 -0300

    edutec

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "A primeira espécie reconhecida como capaz de fabricar artefatos de pedra é chamada de:",
+        answer: "C. Homo habilis",
+        options: [
+            "A. Homo ergaster",
+            "B. Homo erectus",
+            "C. Homo habilis",
+            "D. Homo sapiens"
+        ]
+    },
+    {
+        numb: 2,
+        question: "O esqueleto mais antigo encontrado no Brasil da espécie Homo sapiens viveu há cerca de 11 mil anos e é conhecido pelo nome de:",
+        answer: "A. Luzia",
+        options: [
+            "A. Luzia",
+            "B. Naia",
+            "C. Lúcia",
+            "D. Lucy"
+        ]
+    },
+    {
+        numb: 3,
+        question: "A especiação do Homo sapiens tem pouca chance de ocorrer, considerando-se a atual condição da espécie humana. Assinale a afirmação que MELHOR sustenta essa hipótese.",
+        answer: "A. Os meios de locomoção e de comunicação tem diminuido ou eliminado os isolamentos  geográficos",
+        options: [
+            "A. Os meios de locomoção e de comunicação tem diminuido ou eliminado os isolamentos  geográficos",
+            "B. Os medicamentos atuais diminuem a incidencia de doenças",
+            "C. A ciencia moderna tem eliminado as mutaçoão humanas",
+            "D. Os postulados de Darwin não se aplicam á espécie humana"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Recentes análises do DNA de chimpanzés permitiram concluir que o homem é mais aparentado com eles do que com qualquer outro primata. Isso permite concluir que:        ",
+        answer: "D. O chimpanzé e o homem tem um ancestral comum",
+        options: [
+            "A. A evolução do homem não foi gradual",
+            "B. O homem e o chimpanzé são ancestrais dos gorilas",
+            "C. O chimpanzé é ancestral do homem",
+            "D. O chimpanzé e o homem tem um ancestral comum"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Foram feitas comparações entre DNA e proteínas da espécie humana com DNA e proteínas de diversos primatas. Observando a árvore filogenética, você espera que os dados bioquímicos tenham apontado, entre os primatas atuais, como nosso parente mais próximo o:",
+        answer: "D. Chimpanzé.",
+        options: [
+            "A. Australopithecus.",
+            "B. Ramapithecus.",
+            "C. Gorila.",
+            "D. Chimpanzé."
+        ]
+    }
+];
